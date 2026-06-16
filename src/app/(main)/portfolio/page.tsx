9c535481933243cc0b1b445cd2287d0594e35d27
ddezeta <danielzmbrnz@gmail.com>
2026-06-15 19:11:37 -0500

    add Post type, and use map to add many images to grid

--- src/app/(main)/portfolio/page.tsx
+++ src/app/(main)/portfolio/page.tsx
@@ -1,20 +1,42 @@
-import Image from 'next/image';
-
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { Post } from "./types";
 
 export default function Portfolio() {
-    return (
-        <div className="p-4">
-            <h2 className="text-2xl font-bold mb-4">Portfolio</h2>
-            <p className="text-gray-600">Your connected ad accounts and performance metrics will appear here.</p>
-            <div className="grid grid-cols-3 gap-1">
-                <Image src="/dz_content/djspike.png" alt="DJ spike" width={500} height={300}/>
-                <Image src="/dz_content/photo1.jpg" alt="DJ spike" width={500} height={300}/>
-                <Image src="/dz_content/post1.jpg" alt="DJ spike" width={500} height={300}/>
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeImage, setActiveImage] = useState("");
 
-                <Image src="/dz_content/post2.jpg" alt="DJ spike" width={500} height={300}/>
-                <Image src="/dz_content/post3.jpg" alt="DJ spike" width={500} height={300}/>
-                <Image src="/dz_content/lonsovilla_still_fire1.png" alt="DJ spike" width={500} height={300}/>
-            </div>
-        </div>
-    );
+  const posts: Post[] = [
+    { id: "1", imageUrl: "/dz_content/djspike.png", author: "dezeta" },
+    { id: "2", imageUrl: "/dz_content/photo1.jpg", author: "dezeta" },
+    { id: "3", imageUrl: "/dz_content/post1.jpg", author: "dezeta" },
+    { id: "4", imageUrl: "/dz_content/post2.jpg", author: "dezeta" },
+    { id: "5", imageUrl: "/dz_content/post3.jpg", author: "dezeta" },
+    {
+      id: "6",
+      imageUrl: "/dz_content/lonsovilla_still_fire1.png",
+      author: "dezeta",
+    },
+  ];
+
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Portfolio</h2>
+      <p className="text-gray-600">
+        Your connected ad accounts and performance metrics will appear here.
+      </p>
+      <div className="grid grid-cols-3 gap-1" onClick={() => setIsOpen(true)}>
+        {posts.map((post) => (
+          <Image
+            key={post.id}
+            src={post.imageUrl}
+            alt={post.author}
+            width={300}
+            height={300}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
